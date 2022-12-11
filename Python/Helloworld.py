8507e213d5a9989038546ef59ae9bf6c42048086
Mohamed Mansour <mohamed.mansour1472011@gmail.com>
2022-12-11 11:21:22 +0100

    commited two different files

---
+++ Python/Helloworld.py
@@ -0,0 +1,14 @@
+#this prints some text
+print("Hello world")
+print("I am Mohamed Mansour.I am a Basketball player.I play Basketball for BasketDuisburg.")
+
+
+Basket_Ball = input("What do you wanna put in the Basket_Ball variable? ")
+print(Basket_Ball)
+
+name = input("What is your name ? ")
+print("Hello " + name)
+
+Birth_year = input("Enter Birth year ")
+age = 2022 - int(Birth_year)
+print(age)

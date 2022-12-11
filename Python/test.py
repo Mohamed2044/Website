9b993ed361eec7b134814ab46b4574a6d797cf6c
Mohamed Mansour <mohamed.mansour1472011@gmail.com>
2022-12-11 12:08:28 +0100

    commited a new file

---
+++ Python/test.py
@@ -0,0 +1,14 @@
+Mohamed_age = 12
+Basket_Duisburg_U14 = 12
+
+print(Mohamed_age == Basket_Duisburg_U14)
+
+if Mohamed_age < Basket_Duisburg_U14:
+    print("Mohamed should stay at Basket Duisburg U12")
+elif Mohamed_age == Basket_Duisburg_U14:
+    print("Enjoy Basket Duisburg U14")
+
+else:
+    print("Mohamed should go to Basket Duisburg U14")
+    
+    

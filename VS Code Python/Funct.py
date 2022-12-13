a92b4b5eba9dd3d3816d3385780ee788bd0b6752
Mohamed Mansour <mohamed.mansour1472011@gmail.com>
2022-12-13 16:22:25 +0100

    Cimmited Files to a new Folder

---
+++ VS Code Python/Funct.py
@@ -0,0 +1,11 @@
+def print_Mohamed():
+    text = "Mohamed is a great Basketball player"
+    print(text)
+    print(text)
+    print(text)
+    print(text)
+    
+print_Mohamed()
+
+
+
---
+++ VS Code Python/Helloworld.py
@@ -0,0 +1,8 @@
+#this prints some text
+print("Hello world")
+print("I am Mohamed Mansour.I am a Basketball player.I play Basketball for BasketDuisburg.")
+
+
+Basket_Ball = input("What do you wanna put in the Basket_Ball variable? ")
+print(Basket_Ball)
+
